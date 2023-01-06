1effd94c9f9421061d0f5f9c0dc1adc183e4fbc2
Ashiya Kandhway <ashiyakandhway27july@gmail.com>
2023-01-06 21:11:25 +0530

    skills added

--- lib/Screens/details.dart
+++ lib/Screens/details.dart
@@ -3,9 +3,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:student_assistant_app/Screens/home.dart';
-import 'package:student_assistant_app/Screens/skillwidget.dart';
 import 'package:student_assistant_app/utilities/constants.dart';
-
+import 'package:get/get.dart';
+import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
+import 'package:student_assistant_app/Controller/skills.dart';
+import 'package:student_assistant_app/utilities/Skill.dart';
 class Details extends StatefulWidget {
   const Details({Key? key}) : super(key: key);
 
@@ -163,6 +165,13 @@ class _DetailsState extends State<Details> {
           print(_branch.text);
           print(int.parse(_date.text));
           print(double.parse(_cgpa.text));
+         // print("here");
+         List<String> skills=[];
+          for(var i=0;i<skillData.length;i++){
+            print(skillData[i].skillname);
+            skills.add(skillData[i].skillname);
+            print(skills[i]);
+          }
           await FirebaseFirestore.instance
               .collection('users')
               .doc(FirebaseAuth.instance.currentUser?.uid)
@@ -173,7 +182,7 @@ class _DetailsState extends State<Details> {
             "College": _college.text,
             "Major": _branch.text,
             "Gradyear": int.parse(_date.text),
-            "Skills": ["C++", "Java"],
+            "Skills": skills,
           }).then((value) {
             print("Uers data added");
             var _currentUser;
@@ -216,9 +225,77 @@ class _DetailsState extends State<Details> {
       ),
     );
   }
+  List<SkillModel>skillData = [];
+  Widget _buildMultiSelect(){
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Skills',
+            style: kLabelStyle,
+          ),
+          SizedBox(height: 10.0),
+          Container(
+            alignment: Alignment.centerLeft,
+            decoration: kBoxDecorationStyle,
+            // height: 170.0,
+            child: GetBuilder<AppDataController>(
+              builder: (controller) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: MultiSelectDialogField(
+                    dialogHeight: 200,
+                    items: controller.dropDownData,
+                    title: const Text(
+                      "Select skills",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    selectedColor: Colors.blue,
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: const BorderRadius.all(Radius.circular(30)),
+                    ),
+                    buttonIcon: const Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.white,
+                    ),
+                    buttonText: const Text(
+                      "Select your strong skills",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'OpenSans',
+                      ),
+                    ),
+                    onConfirm: (results) {
+                      //skillData = [];
+                      for (var i = 0; i < results.length; i++) {
+                        SkillModel data = results[i] as SkillModel;
+                        print(data.skillname);
+                        print(data.skillid);
+                        skillData.add(data);
+                      }
+                      //skillData=results;
+                      print(" here data $skillData");
+                    },
+                  ),
+                );
+              },
+            ),
+          )
+        ]);
+  }
+
+  final AppDataController controller = Get.put(AppDataController());
 
   @override
+
   Widget build(BuildContext context) {
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      controller.getSkillData();
+    });
     return Scaffold(
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
@@ -277,7 +354,7 @@ class _DetailsState extends State<Details> {
                       SizedBox(height: 30.0),
                       _buildCareers(),
                       SizedBox(height: 30.0),
-                      MultiSelectDropDownScreen(),
+                      _buildMultiSelect(),
                       SizedBox(height: 30.0),
                       _buildSubmitButton()
                     ],
